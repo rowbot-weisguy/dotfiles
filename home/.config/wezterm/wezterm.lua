b30f9b4ee011344f83c6dd65a76568b6afef64a8
Rowan Weismiller <rowan.weismiller@spexi.com>
2026-07-12 18:05:45 -0700

    Added cursor movement hotkeys to wezterm config

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -10,4 +10,37 @@ config.macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 
+config.keys = {
+  {
+    key = 'm',
+    mods = 'CMD',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = 'LeftArrow',
+    mods = 'CMD',
+    action = wezterm.action.SendString '\x01',
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CMD',
+    action = wezterm.action.SendString '\x05',
+  },
+  {
+    key = 'LeftArrow',
+    mods = 'OPT',
+    action = wezterm.action.SendString '\x1bb',
+  },
+  {
+    key = 'RightArrow',
+    mods = 'OPT',
+    action = wezterm.action.SendString '\x1bf',
+  },
+  {
+    key = 'Backspace',
+    mods = 'CMD',
+    action = wezterm.action.SendString '\x15',
+  },
+}
+
 return config
