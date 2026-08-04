102445efdd167ed565ea9473aab940598dc29841
Rowan Weismiller <rowan.weismiller@spexi.com>
2026-08-04 11:31:53 -0700

    Disable arrow keys in nvim

--- home/.config/nvim/lua/keys.lua
+++ home/.config/nvim/lua/keys.lua
@@ -1,5 +1,9 @@
 -- save by pressing Escape
 vim.keymap.set('n', '<Esc>', ':w<CR>', { desc = 'Save' })
+-- force hjkl navigation by unbinding the arrow keys
+for _, key in ipairs({ '<Up>', '<Down>', '<Left>', '<Right>' }) do
+  vim.keymap.set({ 'n', 'i', 'v' }, key, '<Nop>', { desc = 'Disabled: use hjkl' })
+end
 -- select all
 vim.keymap.set('n', '<C-a>', 'ggVG', { desc = 'Select All' })
 -- pasting over a selection no longer clobbers your clipboard
